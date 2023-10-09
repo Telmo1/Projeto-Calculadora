e1f9d9fea3f0e544d1232863a07907e934cd7f19
Telmo <yrtelmo00@gmail.com>
2023-10-08 21:50:37 -0300

    Resolvendo bugs de responsividade

--- JS/script.js
+++ JS/script.js
@@ -1,26 +1,32 @@
-function insert(num)
-{
-    var numero = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = numero + num;
+var visor;
+var resultado;
+
+// Função para adicionar números e operadores ao visor
+function adicionarAoVisor(valor) {
+    visor = document.getElementById('resultado');
+    visor.innerHTML += valor;
 }
-function clean()
-{
-    document.getElementById('resultado').innerHTML = "";
+
+// Função para limpar o visor
+function limparVisor() {
+    visor = document.getElementById('resultado');
+    visor.innerHTML = "";
 }
-function back()
-{
-    var resultado = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+
+// Função para remover o último caractere do visor
+function removerUltimoCaractere() {
+    visor = document.getElementById('resultado');
+    visor.innerHTML = visor.innerHTML.slice(0, -1);
 }
-function calcular()
-{
-    var resultado = document.getElementById('resultado').innerHTML;
-    if(resultado)
-    {
-        document.getElementById('resultado').innerHTML = eval(resultado);
-    }
-    else
-    {
-        document.getElementById('resultado').innerHTML = "Nada..."
+
+// Função para calcular e exibir o resultado
+function calcularResultado() {
+    visor = document.getElementById('resultado');
+    resultado = visor.innerHTML;
+
+    if(resultado) {
+        visor.innerHTML = eval(resultado);
+    } else {
+        visor.innerHTML = "Nada...";
     }
 }
